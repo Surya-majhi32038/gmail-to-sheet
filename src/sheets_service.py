1852cd259470a1b72bda051557506dc2c3f1ce96
Surya-majhi32038 <suryamajhi32038@gmail.com>
2026-01-19 19:47:21 +0530

    content update : fetch data from content

--- src/sheets_service.py
+++ src/sheets_service.py
@@ -1,6 +1,8 @@
+from bs4 import BeautifulSoup
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
-
+from email.utils import parseaddr
+from email.utils import parsedate_to_datetime
 from config import (
     GMAIL_SCOPES,
     TOKEN_FILE,
@@ -27,15 +29,69 @@ def get_sheets_service():
 # ---------------------------------
 
 def format_row_data(parsed_email):
+    
     """
     Convert parsed email dictionary into a row list.
     """
+    from_data = parsed_email.get("from", "")
+    email_address = parseaddr(from_data)[1]
+
+    # date 
+    
+
+    date_data = parsed_email.get("date", "")
+
+    try:
+        date_obj = parsedate_to_datetime(date_data)
+        date_obj = date_obj.replace(tzinfo=None)
+        formatted_date = date_obj.strftime("%Y-%m-%d %H:%M:%S")
+    except Exception:
+        formatted_date = "Unknown"
+
+
+    payload = parsed_email.get("content", {})
+   
+    email_text = clean_email_content(payload)
+  
+
     return [
-        parsed_email.get("from", ""),
-        parsed_email.get("subject", ""),
-        parsed_email.get("date", ""),
-        parsed_email.get("content", "")
+        email_address,
+        parsed_email.get("subject", "").strip(),
+        formatted_date,
+        email_text
     ]
+
+
+import re
+
+def clean_email_content(html: str) -> str:
+    if not html:
+        return ""
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    #  remove unwanted tags completely
+    for tag in soup(["style", "script", "head", "meta", "title", "img"]):
+        tag.decompose()
+
+    #  get readable text
+    text = soup.get_text(separator="\n")
+
+    #  clean junk
+    text = re.sub(r"\n{2,}", "\n\n", text)
+    text = re.sub(r"[ \t]{2,}", " ", text)
+
+    # remove leftover links
+    text = re.sub(r"https?://\S+", "", text)
+
+    #  remove image markers
+    text = re.sub(r"\[image:[^\]]*\]", "", text, flags=re.I)
+
+    # final cleanup
+    lines = [line.strip() for line in text.split("\n") if line.strip()]
+
+    return "\n".join(lines)
+
 
 
 # ---------------------------------
